b88376c7993aaa191f35247efd9aa095846e1e89
MS201510 <chuams-wm22@student.tarc.edu.my>
2025-07-23 15:23:28 +0800

    Initial commit without locked DB files

---
+++ Demo/Models/ViewModels.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models;
+
+#nullable disable warnings
+
+// View Models ----------------------------------------------------------------
+
+public class LoginVM
+{
+    [StringLength(100)]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [StringLength(100, MinimumLength = 5)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public bool RememberMe { get; set; }
+}
+
+public class RegisterVM
+{
+    [StringLength(100)]
+    [EmailAddress]
+    [Remote("CheckEmail", "Account", ErrorMessage = "Duplicated {0}.")]
+    public string Email { get; set; }
+
+    [StringLength(100, MinimumLength = 5)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [StringLength(100, MinimumLength = 5)]
+    [Compare("Password")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password")]
+    public string Confirm { get; set; }
+
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    public IFormFile Photo { get; set; }
+}
+
+public class UpdatePasswordVM
+{
+    [StringLength(100, MinimumLength = 5)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Current Password")]
+    public string Current { get; set; }
+
+    [StringLength(100, MinimumLength = 5)]
+    [DataType(DataType.Password)]
+    [Display(Name = "New Password")]
+    public string New { get; set; }
+
+    [StringLength(100, MinimumLength = 5)]
+    [Compare("New")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password")]
+    public string Confirm { get; set; }
+}
+
+public class UpdateProfileVM
+{
+    public string? Email { get; set; }
+
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    public string? PhotoURL { get; set; }
+
+    public IFormFile? Photo { get; set; }
+}
+
+public class ResetPasswordVM
+{
+    [StringLength(100)]
+    [EmailAddress]
+    public string Email { get; set; }
+}
+
+// Reservation
+
+public class ReserveVM
+{
+    [Display(Name = "Room Type")]
+    public string TypeId { get; set; }
+
+    [Display(Name = "Check In Date")]
+    [DataType(DataType.Date)]
+    public DateOnly CheckIn { get; set; }
+
+    [Display(Name = "Check Out Date")]
+    [DataType(DataType.Date)]
+    public DateOnly CheckOut { get; set; }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,21 @@
+global using Demo.Models;
+global using Demo;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddSqlServer<DB>($@"
+    Data Source=(LocalDB)\MSSQLLocalDB;
+    AttachDbFilename={builder.Environment.ContentRootPath}\DB.mdf;
+");
+builder.Services.AddScoped<Helper>();
+builder.Services.AddAuthentication().AddCookie();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRequestLocalization("en-MY");
+app.UseSession();
+app.MapDefaultControllerRoute();
+app.Run();
